7e440d1f8d832d2ffb09937a3d75396d3f61b4bc
Kajetan <drakles1@gmail.com>
2019-01-17 22:38:46 +0100

    initial commit

---
+++ src/main/java/Anonimize/App.java
@@ -0,0 +1,49 @@
+package Anonimize;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+import javax.imageio.ImageIO;
+
+public class App {
+
+  public static void main(String[] args) {
+
+    Scanner in = new Scanner(System.in);
+    System.out.println("Path:");
+
+    String dataPath = in.next();
+    System.out.println("Top cut:");
+    int cut = in.nextInt();
+
+    Path path = Paths.get(dataPath);
+
+    try {
+      Files.walk(path).filter(pa -> pa.toFile().isFile() && pa.toFile().getName().endsWith(".tif"))
+          .map(Path::toFile)
+          .forEach(p -> convert(p, cut));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private static void convert(File image, int topCutPixel) {
+    try {
+      BufferedImage bfImage = ImageIO.read(image);
+      ImageIO.write(cropImage(bfImage, bfImage.getMinX(), topCutPixel, bfImage.getWidth(),
+          bfImage.getHeight() - topCutPixel), "tif", image);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private static BufferedImage cropImage(BufferedImage bufferedImage, int x, int y, int width,
+      int height) {
+    return bufferedImage.getSubimage(x, y, width, height);
+  }
+
+}
