7aaf3a805a710bd817c5b019d405aad7f2723592
Kajetan <drakles1@gmail.com>
2019-08-30 21:35:58 +0200

    add to CNN format converter

--- src/main/java/Anonimize/App.java
+++ src/main/java/Anonimize/App.java
@@ -1,5 +1,6 @@
 package Anonimize;
 
+import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -7,49 +8,58 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Scanner;
-import javax.imageio.ImageIO;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class App {
 
-  public static void main(String[] args) {
+    private static AtomicInteger counter = new AtomicInteger();
 
-    Scanner in = new Scanner(System.in);
-    System.out.println("Path:");
+    public static void main(String[] args) {
+        App.counter.set(0);
 
-    String dataPath = in.next();
-    System.out.println("Top cut:");
-    int cut = in.nextInt();
+        System.out.println("Anonimize? true/false:");
+        Scanner in = new Scanner(System.in);
+        final int anonimizeCut = in.nextBoolean() ? 100 : 0;
 
-    Path path = Paths.get(dataPath);
+        System.out.println("To CNN format? true/false");
+        final boolean isMinimize = in.nextBoolean();
 
-    System.out.println("Absolute path to data folder: " + path.toAbsolutePath().toString());
-
-    try {
-      Files.walk(path).filter(pa -> pa.toFile().isFile() && pa.toFile().getName().endsWith(".tif"))
-          .map(Path::toFile)
-          .forEach(p -> convert(p, cut));
-    } catch (IOException e) {
-      e.printStackTrace();
+        final Path path = Paths.get("data/input");
+        try {
+            Files.walk(path).filter(pa -> pa.toFile().isFile() && pa.toFile().getName().endsWith(".tif"))
+                    .map(Path::toFile)
+                    .forEach(p -> convert(p, anonimizeCut, App.counter.addAndGet(1), isMinimize));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Total files created:" + counter);
     }
-  }
 
-  private static void convert(File image, int topCutPixel) {
-    try {
-      BufferedImage bfImage = ImageIO.read(image);
-      if(bfImage != null) {
-        ImageIO.write(cropImage(bfImage, bfImage.getMinX(), topCutPixel, bfImage.getWidth(),
-            bfImage.getHeight() - topCutPixel), "tif", image);
-      }else {
-        System.out.println("Image is null: " + image.getPath());
-      }
-    } catch (IOException e) {
-      e.printStackTrace();
+    private static void convert(File image, int topCutPixel, int counterId, boolean isToMinimize) {
+        try {
+            BufferedImage bfInputImage = ImageIO.read(image);
+            File outputFile = new File("data/output/" + counterId + image.getName());
+            if (bfInputImage != null) {
+                int x, y, w, h;
+                if (isToMinimize) {
+                    x = 400;
+                    y = 405 + topCutPixel;
+                    w = 245;
+                    h = w;
+                } else {
+                    x = bfInputImage.getMinX();
+                    y = topCutPixel;
+                    w = bfInputImage.getWidth();
+                    h = bfInputImage.getHeight() - topCutPixel;
+                }
+
+              ImageIO.write(bfInputImage.getSubimage(x, y, w, h), "tif", outputFile);
+            } else {
+                System.out.println("Image is null: " + image.getPath());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-  }
-
-  private static BufferedImage cropImage(BufferedImage bufferedImage, int x, int y, int width,
-      int height) {
-    return bufferedImage.getSubimage(x, y, width, height);
-  }
 
 }
