1f388980f5161ee195de8b81a4f38cbca5f61418
Kajetan <drakles1@gmail.com>
2019-10-27 17:30:10 +0100

    add formatter

--- src/main/java/Anonimize/App.java
+++ src/main/java/Anonimize/App.java
@@ -1,6 +1,5 @@
 package Anonimize;
 
-import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -9,57 +8,59 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Scanner;
 import java.util.concurrent.atomic.AtomicInteger;
+import javax.imageio.ImageIO;
 
 public class App {
 
-    private static AtomicInteger counter = new AtomicInteger();
+  private static AtomicInteger counter = new AtomicInteger(0);
 
-    public static void main(String[] args) {
-        App.counter.set(0);
+  public static void main(String[] args) {
+    App.counter.set(0);
 
-        System.out.println("Anonimize? true/false:");
-        Scanner in = new Scanner(System.in);
-        final int anonimizeCut = in.nextBoolean() ? 100 : 0;
+    System.out.println("Anonimize? true/false:");
+    Scanner in = new Scanner(System.in);
+    final int anonimizeCut = in.nextBoolean() ? 100 : 0;
 
-        System.out.println("To CNN format? true/false");
-        final boolean isMinimize = in.nextBoolean();
+    System.out.println("To CNN format? true/false");
+    final boolean isToCNNFormat = in.nextBoolean();
 
-        final Path path = Paths.get("data/input");
-        try {
-            Files.walk(path).filter(pa -> pa.toFile().isFile() && pa.toFile().getName().endsWith(".tif"))
-                    .map(Path::toFile)
-                    .forEach(p -> convert(p, anonimizeCut, App.counter.addAndGet(1), isMinimize));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        System.out.println("Total files created:" + counter);
+    final Path path = Paths.get("data/input");
+    try {
+      Files.walk(path)
+          .filter(pa -> pa.toFile().isFile() && pa.toFile().getName().endsWith(".tif"))
+          .map(Path::toFile)
+          .forEach(p -> convert(p, anonimizeCut, App.counter.addAndGet(1), isToCNNFormat));
+    } catch (IOException e) {
+      e.printStackTrace();
+    } finally {
+      System.out.println("Total files created:" + counter);
     }
+  }
 
-    private static void convert(File image, int topCutPixel, int counterId, boolean isToMinimize) {
-        try {
-            BufferedImage bfInputImage = ImageIO.read(image);
-            File outputFile = new File("data/output/" + counterId + image.getName());
-            if (bfInputImage != null) {
-                int x, y, w, h;
-                if (isToMinimize) {
-                    x = 400;
-                    y = 405 + topCutPixel;
-                    w = 245;
-                    h = w;
-                } else {
-                    x = bfInputImage.getMinX();
-                    y = topCutPixel;
-                    w = bfInputImage.getWidth();
-                    h = bfInputImage.getHeight() - topCutPixel;
-                }
+  private static void convert(File image, int topCutPixel, int counterId, boolean isToMinimize) {
+    try {
+      final BufferedImage bfInputImage = ImageIO.read(image);
+      final File outputFile = new File("data/output/" + counterId + image.getName());
 
-              ImageIO.write(bfInputImage.getSubimage(x, y, w, h), "tif", outputFile);
-            } else {
-                System.out.println("Image is null: " + image.getPath());
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+      if (bfInputImage != null) {
+        int x, y, w, h;
+        if (isToMinimize) {
+          x = 200;
+          y = 330 + topCutPixel;
+          w = 630;
+          h = 330;
+        } else {
+          x = bfInputImage.getMinX();
+          y = topCutPixel;
+          w = bfInputImage.getWidth();
+          h = bfInputImage.getHeight() - topCutPixel;
         }
+        ImageIO.write(bfInputImage.getSubimage(x, y, w, h), "tif", outputFile);
+      } else {
+        System.out.println("Image is null: " + image.getPath());
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
     }
-
+  }
 }
