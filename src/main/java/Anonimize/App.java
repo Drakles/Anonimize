5d57d069cd3d8c9ed10837fcb46f98dbfe5bdd6a
Kajetan <drakles1@gmail.com>
2019-11-21 11:16:42 +0100

    add error handling

--- src/main/java/Anonimize/App.java
+++ src/main/java/Anonimize/App.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.concurrent.atomic.AtomicInteger;
 import javax.imageio.ImageIO;
@@ -17,34 +18,63 @@ public class App {
   public static void main(String[] args) {
     App.counter.set(0);
 
-    System.out.println("Anonimize? true/false:");
-    Scanner in = new Scanner(System.in);
-    final int anonimizeCut = in.nextBoolean() ? 100 : 0;
-
-    System.out.println("To CNN format? true/false");
-    final boolean isToCNNFormat = in.nextBoolean();
-
-    final Path path = Paths.get("data/input");
     try {
+      final Scanner in = new Scanner(System.in);
+      System.out.println("Use custom folder for input data? default is 'data/input' true/false");
+
+      Path path;
+      if (in.nextBoolean()) {
+        System.out.println("Specify path for custom folder");
+        path = Paths.get(in.next());
+      } else {
+        path = Paths.get("data/input");
+      }
+
+      System.out.println("Anonimize images? true/false:");
+      final int anonimizeCut = in.nextBoolean() ? 100 : 0;
+
+      System.out.println("Convert to format accepted by model? true/false");
+      final boolean isToModelFormat = in.nextBoolean();
+
+      System.out.println("Use custom image extension? default is 'tif' true/false");
+      String imageExtension;
+      if (in.nextBoolean()) {
+        System.out.println("Specify image extension");
+        imageExtension = in.next();
+      } else {
+        imageExtension = "tif";
+      }
+
       Files.walk(path)
-          .filter(pa -> pa.toFile().isFile() && pa.toFile().getName().endsWith(".tif"))
+          .filter(pa -> pa.toFile().isFile() && pa.toFile().getName().endsWith(imageExtension))
           .map(Path::toFile)
-          .forEach(p -> convert(p, anonimizeCut, App.counter.addAndGet(1), isToCNNFormat));
+          .forEach(
+              image ->
+                  convert(
+                      image,
+                      anonimizeCut,
+                      App.counter.addAndGet(1),
+                      isToModelFormat,
+                      imageExtension));
     } catch (IOException e) {
       e.printStackTrace();
+    } catch (InputMismatchException e) {
+      System.out.println(
+          "Please answer only 'true' or 'false' if question specify 'true/false' at the end of the message");
     } finally {
       System.out.println("Total files created:" + counter);
     }
   }
 
-  private static void convert(File image, int topCutPixel, int counterId, boolean isToMinimize) {
+  private static void convert(
+      File image, int topCutPixel, int counterId, boolean isToModelFormat, String imageExtension) {
     try {
       final BufferedImage bfInputImage = ImageIO.read(image);
       final File outputFile = new File("data/output/" + counterId + image.getName());
 
       if (bfInputImage != null) {
         int x, y, w, h;
-        if (isToMinimize) {
+        if (isToModelFormat) {
           x = 200;
           y = 330 + topCutPixel;
           w = 630;
@@ -55,9 +85,9 @@ public class App {
           w = bfInputImage.getWidth();
           h = bfInputImage.getHeight() - topCutPixel;
         }
-        ImageIO.write(bfInputImage.getSubimage(x, y, w, h), "tif", outputFile);
+        ImageIO.write(bfInputImage.getSubimage(x, y, w, h), imageExtension, outputFile);
       } else {
-        System.out.println("Image is null: " + image.getPath());
+        System.out.println("Image in directory" + image.getPath() + "is null");
       }
     } catch (IOException e) {
       e.printStackTrace();
